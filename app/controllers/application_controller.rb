9b6f6286b0bb9c8cb148f16861c289591927af07
Ward Verhoef <ward.verhoef@gmail.com>
2016-06-11 03:29:11 +0200

    generated migration add_user_id_to_statuses, removed old statuses in irb console, tryed to add attr_accessbile to user.rb but found out it got replace by latest ruby version and instead added 3 methods(create update and private  erson_params to ApplicationsController class. but  now i cant create new post anymore. i know its an easy fix but i cant find it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def create
+   Person.create(person_params)
+ end
+
+ def update
+   redirect_to current_account.people.find(params[:id]).tap { |person|
+     person.update!(person_params)
+   }
+ end
+ private
+
+ def person_params
+      params.require(:person).permit(:name, :age)
+    end
 end
